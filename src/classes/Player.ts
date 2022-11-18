4176e6dc1909ae20b5e5392995bb2b7cfb988138
Juan Eduardo Villegas Rios <A00826615@itesm.mx>
2022-11-18 11:26:14 -0600

    walking thingy

---
+++ src/classes/Player.ts
@@ -0,0 +1,81 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+
+export class Player {
+
+    private model: THREE.Group
+    private mixer: THREE.AnimationMixer
+    private animationsMap: Map<string, THREE.AnimationAction> = new Map() // Walk, Run, Idle
+    private orbitControl: OrbitControls
+    private camera: THREE.Camera
+    // state
+    private toggleRun: boolean = true
+    private currentAction: string
+     // temporary data
+    private walkDirection = new THREE.Vector3()
+    private rotateAngle = new THREE.Vector3(0, 1, 0)
+    private rotateQuarternion: THREE.Quaternion = new THREE.Quaternion()
+    private cameraTarget = new THREE.Vector3()
+
+      // constants
+    private readonly fadeDuration: number = 0.2
+    private readonly runVelocity:number = .4
+    private readonly walkVelocity:number = .1
+
+    constructor(model: THREE.Group, 
+        mixer: THREE.AnimationMixer,  
+        animationsMap: Map<string, THREE.AnimationAction>,
+        orbitControl: OrbitControls, 
+        camera: THREE.Camera,
+        currentAction: string) {
+        this.model = model
+        this.mixer = mixer
+        this.animationsMap = animationsMap
+        this.currentAction = currentAction
+        this.animationsMap.forEach((value, key) => {
+            if (key == currentAction) {
+                value.play()
+            }
+        })
+        this.orbitControl = orbitControl
+        this.camera = camera
+    }
+
+    public switchRunToggle() : void {
+        this.toggleRun = !this.toggleRun
+    }
+
+    
+    public update(delta:number, keysPressed:any) : void{
+        console.log(keysPressed.w)
+        const directionPressed = ['w','a','s','d'].some(key => keysPressed[key] == true)
+        let play = ''
+        if (directionPressed && this.toggleRun) {
+            play = 'walk'
+        } else if (directionPressed) {
+            play = 'run.001' //walking
+        } else {
+            play = 'idle'
+        }
+
+        if (this.currentAction != play) {
+            const toPlay= this.animationsMap.get(play)
+            const current = this.animationsMap.get(this.currentAction)
+
+            current?.fadeOut(this.fadeDuration)
+            toPlay?.reset().fadeIn(this.fadeDuration).play()
+            this.currentAction = play
+        }
+        this.mixer.update(delta)
+
+        if (this.currentAction == 'run.001' || this.currentAction == 'walk') {
+            // run/walk velocity
+            const velocity = this.currentAction == 'run.001' ? this.runVelocity : this.walkVelocity
+
+            this.model.position.x += velocity
+            this.model.position.z += velocity
+        }
+    }
+
+}
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,49 +1,42 @@
 import * as THREE from 'three'
 import * as CANNON from 'cannon-es'
+import { Player } from '../classes/Player'
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+
+//Scene
 const scene = new THREE.Scene()
-scene.add(new THREE.AxesHelper(5))
+scene.background = new THREE.Color(0xa8def0);
 
-const light1 = new THREE.SpotLight()
-light1.position.set(2.5, 5, 5)
-light1.angle = Math.PI / 4
-light1.penumbra = 0.5
-light1.castShadow = true
-light1.shadow.mapSize.width = 1024
-light1.shadow.mapSize.height = 1024
-light1.shadow.camera.near = 0.5
-light1.shadow.camera.far = 20
-scene.add(light1)
-
-const light2 = new THREE.SpotLight()
-light2.position.set(-2.5, 5, 5)
-light2.angle = Math.PI / 4
-light2.penumbra = 0.5
-light2.castShadow = true
-light2.shadow.mapSize.width = 1024
-light2.shadow.mapSize.height = 1024
-light2.shadow.camera.near = 0.5
-light2.shadow.camera.far = 20
-scene.add(light2)
-
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
+//Camera
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000)
 camera.position.set(0, 2, 10)
 
-const renderer = new THREE.WebGLRenderer()
+//Renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.shadowMap.enabled = true
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement)
+renderer.shadowMap.enabled = true
 
+// Contorls
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true
+orbitControls.minDistance = 5
+orbitControls.maxDistance = 15
+orbitControls.enablePan = false
+orbitControls.maxPolarAngle = Math.PI / 2 - 0.05
+orbitControls.update();
 
+//World
 const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
-
+//Light
+light()
+//Geometry
 const normalMaterial = new THREE.MeshNormalMaterial()
 const phongMaterial = new THREE.MeshPhongMaterial()
 
@@ -53,6 +46,7 @@ cubeMesh.position.x = -3
 cubeMesh.position.y = 3
 cubeMesh.castShadow = true
 scene.add(cubeMesh)
+
 const cubeShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5))
 const cubeBody = new CANNON.Body({ mass: 1 })
 cubeBody.addShape(cubeShape)
@@ -72,25 +66,51 @@ planeBody.addShape(planeShape)
 planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
 world.addBody(planeBody)
 
-window.addEventListener('resize', onWindowResize, false)
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.render(scene, camera)
-}
 
 
+let player: Player
+const loader = new GLTFLoader()
+loader.load('/models/warlock.glb',function (gltf) {
+    const model = gltf.scene
+    model.traverse(function(object: any){
+        if(object.isMesh) object.castShadow = true
+    })
+    const gltfAnimations: THREE.AnimationClip[] = gltf.animations
+    const mixer = new THREE.AnimationMixer(model)
+    const animationMap: Map<string, THREE.AnimationAction> = new Map()
+    gltfAnimations.filter(a=> a.name != 'Armature.001|mixamo.com|Layer0').forEach((a:THREE.AnimationClip)=>{
+        animationMap.set(a.name,mixer.clipAction(a))
+    })
+    scene.add(model)
+    player = new Player(model,mixer,animationMap,orbitControls,camera,'idle')
+    }
+)
 
 const clock = new THREE.Clock()
 let delta
+let w = false;
+const zSpeed = .4;
+
+const keysPressed  = { }
+window.addEventListener("keydown", (event) => {
+    if(event.shiftKey && player){
+        player.switchRunToggle()
+    }else{
+        (keysPressed as any)[event.key.toLowerCase()] = true
+    }
+    event.preventDefault();
+}, false)
+document.addEventListener('keyup', (event) => {
+    (keysPressed as any)[event.key.toLowerCase()] = false
+}, false)
+
 
 function animate() {
-    requestAnimationFrame(animate)
-    //delta = clock.getDelta()
-    delta = Math.min(clock.getDelta(), 0.1)
-    world.step(delta)
+    delta = clock.getDelta()
+    world.step(Math.min(delta, 0.1))
+    if(player){
+        player.update(delta,keysPressed)
+    }
     
     // Copy coordinates from Cannon to Three.js
     cubeMesh.position.set(
@@ -104,9 +124,42 @@ function animate() {
         cubeBody.quaternion.z,
         cubeBody.quaternion.w
     )
+    orbitControls.update()
     renderer.render(scene, camera)
+    requestAnimationFrame(animate)
 }
 
 
 
 animate()
+
+
+
+//Things forgotten by the hand of god
+
+// Lights
+function light() {
+    scene.add(new THREE.AmbientLight(0xffffff, 0.7))
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1)
+    dirLight.position.set(- 60, 100, - 10);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.bottom = - 50;
+    dirLight.shadow.camera.left = - 50;
+    dirLight.shadow.camera.right = 50;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 200;
+    dirLight.shadow.mapSize.width = 4096;
+    dirLight.shadow.mapSize.height = 4096;
+    scene.add(dirLight);
+    // scene.add( new THREE.CameraHelper(dirLight.shadow.camera))
+}
+
+// Resize handler
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.render(scene, camera)
+}
+window.addEventListener('resize', onWindowResize, false)
